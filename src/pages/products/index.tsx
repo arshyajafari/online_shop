46769b531a118881113c5961943ba3179e3a0193
Arshya Jafari <arshyajafari11@gmail.com>
2023-03-03 19:31:44 +0330

    Create the beginning of products page

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,57 @@
+// hooks
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// components
+import { ProductCard } from "../../components/ProductCard";
+
+// styled components
+import { Container } from "./Products.style";
+
+// product type
+export type ProductItemType = {
+  id: number;
+  category: string;
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+  amount: number;
+};
+
+export const DisplayProducts = () => {
+  // get products data request method
+  const getProductsData = async (): Promise<ProductItemType[]> =>
+    await (await fetch("https://fakestoreapi.com/products")).json();
+
+  // using query hook
+  const { data, isLoading, error } = useQuery<ProductItemType[]>(
+    "products",
+    getProductsData
+  );
+
+  // get total products in cart method
+  const getTotalItems = () => null;
+
+  // add product to cart method
+  const addToCartHandler = () => null;
+
+  // remove product from cart method
+  const removeFromCartHandler = () => null;
+
+  if (isLoading) console.log("Loading");
+
+  if (error) console.log("Error");
+
+  return (
+    <Container>
+      <div>
+        {data?.map((item) => (
+          <div key={item.id}>
+            <ProductCard item={item} addToCartHandler={addToCartHandler} />
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
+};
