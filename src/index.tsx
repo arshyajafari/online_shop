8ad793a4b9affd7a6c32eac75916c7a09478e119
Arshya Jafari <arshyajafari11@gmail.com>
2023-03-03 17:18:19 +0330

    Import react-query in main index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 // react packages
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 // component
 import App from "./App";
+
+// new object from query client
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
