580ac03e35ae7b7691d0aa80aca772f744c9a7f9
Arshya Jafari <arshyajafari11@gmail.com>
2023-03-03 20:31:16 +0330

    Modify product card styled

--- src/components/ProductCard/Card.style.ts
+++ src/components/ProductCard/Card.style.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  border: 1px solid lightblue;
+  border-radius: 20px;
+`;
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -15,13 +15,27 @@ type props = {
 export const ProductCard: React.FC<props> = ({ item, addToCartHandler }) => {
   return (
     <Container>
-      <img src={item.image} alt={item.title} />
-      <div>
-        <h3>{item.title}</h3>
-        <p>{item.description}</p>
-        <h3>{item.price}</h3>
+      <div className="w-full shadow-lg">
+        <img src={item.image} alt={item.title} className="rounded-t-lg" />
+        <div className="p-6">
+          <h5 className="mb-2 text-xl font-medium text-neutral-700 dark:text-neutral-600">
+            {item.title}
+          </h5>
+          <p className="mb-4 text-base text-gray-500 dark:text-gray-400">
+            {item.description}
+          </p>
+          <h6 className="mb-2 text-lg font-medium leading-tight text-neutral-600 dark:text-neutral-600">
+            ${item.price}
+          </h6>
+        </div>
+        <button
+          type="button"
+          className="w-full rounded-b-lg bg-sky-600 px-6 pt-2.5 pb-2 text-sm font-medium text-white uppercase leading-normal"
+          onClick={() => addToCartHandler(item)}
+        >
+          Add to Cart
+        </button>
       </div>
-      <button onClick={() => addToCartHandler(item)}>Add to Cart</button>
     </Container>
   );
 };
