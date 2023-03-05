370fd211523a18e4e369c355db3ce26d4343b838
Arshya Jafari <arshyajafari11@gmail.com>
2023-03-05 23:28:29 +0330

    Create Cart page

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,13 @@
+// components
+import { TableCM } from "../../components/Table";
+
+// styled components
+import { Container } from "./ShoppingCart.style";
+
+export const DisplayShoppingCart = () => {
+  return (
+    <Container>
+      <TableCM />
+    </Container>
+  );
+};
