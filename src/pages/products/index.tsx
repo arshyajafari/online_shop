0d64c9ded515b25fd12ac1957d2dc8bbd41e1c6c
Arshya Jafari <arshyajafari11@gmail.com>
2023-03-07 09:47:33 +0330

    Modify product margin

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -47,7 +47,7 @@ export const DisplayProducts = () => {
     <Container>
       <div className="grid md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 mb-4">
         {data?.map((item) => (
-          <div key={item.id} className="mt-4 md:mx-4 sm:mx-2">
+          <div key={item.id} className="lg:mx-4 md:mx-3 sm:mx-2 my-2">
             <ProductCard item={item} addToCartHandler={addToCartHandler} />
           </div>
         ))}
